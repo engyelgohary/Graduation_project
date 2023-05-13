a19e7c796ccc769d20041b7024daf7e4ab681715
Your Name <Salah>
2023-05-13 04:23:38 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled1/pages/Login.dart';
 import 'package:untitled1/pages/VerificationCode.dart';
@@ -9,7 +10,10 @@ import 'package:untitled1/pages/setup.dart';
 
 
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
